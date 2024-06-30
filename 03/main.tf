19ddf37ea20d10c75c66acc6103b684143e539cd
koki-develop <kou.pg.0131@gmail.com>
2024-06-30 14:40:32 +0900

    Updated comments and formatting in 03/main.tf to use correct punctuation

--- 03/main.tf
+++ 03/main.tf
@@ -9,10 +9,10 @@ locals {
   lines = split("\n", chomp(var.input))
   aa    = [for a in split(" ", local.lines[1]) : tonumber(a)]
 
-  # a1...an を2進数に変換
+  # a1...an を 2 進数に変換
   bins = [for a in local.aa : format("%b", a)]
 
-  # 2進数の末尾の0の数 = 2 で割れる回数を数える
+  # 2 進数の末尾の 0 の数 = 2 で割れる回数を数える
   counts = [for bin in local.bins : length(regex("0*$", bin))]
 
   # 最小値を取得
