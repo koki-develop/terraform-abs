322b76cfb11df5397db3535212d3d0bd12714bb9
koki-develop <kou.pg.0131@gmail.com>
2024-06-30 09:45:26 +0900

    Add abc086_a

---
+++ abc086_a/main.tf
@@ -0,0 +1,17 @@
+variable "input" {
+  type = string
+}
+
+locals {
+  # a, b を数値に変換
+  ab = split(" ", chomp(var.input))
+  a  = tonumber(local.ab[0])
+  b  = tonumber(local.ab[1])
+
+  # a * b が偶数か奇数か判定
+  result = local.a * local.b % 2 == 0 ? "Even" : "Odd"
+}
+
+output "result" {
+  value = local.result
+}
