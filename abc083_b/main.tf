a5715d9ef5ac13b7f7bfaef9d247345d9aa7f157
koki-develop <kou.pg.0131@gmail.com>
2024-06-30 10:32:49 +0900

    Add abc083_b

---
+++ abc083_b/main.tf
@@ -0,0 +1,24 @@
+variable "input" {
+  type = string
+}
+
+locals {
+  # n, a, b を取得
+  nab = split(" ", chomp(var.input))
+  n   = tonumber(local.nab[0])
+  a   = tonumber(local.nab[1])
+  b   = tonumber(local.nab[2])
+
+  # 各整数の各桁の和を計算
+  sums = [for i in range(1, local.n + 1) : sum([for c in split("", tostring(i)) : tonumber(c)])]
+
+  # 桁の和が A から B の間にある整数のリストを作成
+  valid_numbers = [for i in range(1, local.n + 1) : i if local.sums[i - 1] >= local.a && local.sums[i - 1] <= local.b]
+
+  # 総和を計算
+  result = sum(local.valid_numbers)
+}
+
+output "output" {
+  value = local.result
+}
