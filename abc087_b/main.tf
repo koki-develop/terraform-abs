192c5d698f45b121a3171772068c8dcead8c0322
koki-develop <kou.pg.0131@gmail.com>
2024-06-30 10:20:13 +0900

    Add abc087_b

---
+++ abc087_b/main.tf
@@ -0,0 +1,26 @@
+variable "input" {
+  type = string
+}
+
+locals {
+  # a, b, c, x を取得
+  lines = split("\n", chomp(var.input))
+  a     = tonumber(local.lines[0])
+  b     = tonumber(local.lines[1])
+  c     = tonumber(local.lines[2])
+  x     = tonumber(local.lines[3])
+
+  # 各硬貨の組み合わせを計算
+  combinations = [for a in range(0, local.a + 1) : [
+    for b in range(0, local.b + 1) : [
+      for c in range(0, local.c + 1) : (a * 500 + b * 100 + c * 50 == local.x) ? 1 : 0
+    ]
+  ]]
+
+  # 組み合わせの数を計算
+  result = sum(flatten(local.combinations))
+}
+
+output "result" {
+  value = local.result
+}
