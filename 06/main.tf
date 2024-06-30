6878bc411265990b9b5a151f2724476dc8ca514c
koki-develop <kou.pg.0131@gmail.com>
2024-06-30 14:54:36 +0900

    Update `aa` processing to extract multiple values (a1...an) and refactor card sorting and scoring for Alice and Bob

--- 06/main.tf
+++ 06/main.tf
@@ -5,21 +5,20 @@ variable "input" {
 }
 
 locals {
-  # n, aa を取得
+  # n, a1...an を取得
   lines = split("\n", chomp(var.input))
   n     = tonumber(local.lines[0])
   aa    = [for a in split(" ", local.lines[1]) : tonumber(a)]
 
   # カードを降順にソート、数値に変換
   sorted_cards = [
-    # `sort` は文字列しかサポートしていないため、
-    # 0 埋めすることで数値順のソートを実現
+    # `sort` は文字列しかサポートしていないため、 0 埋めすることで数値順のソートを実現
     for a_str in reverse(sort([
       for a in local.aa : format("%03d", a)
     ])) : tonumber(a_str)
   ]
 
-  # AliceとBobの得点を計算
+  # Alice と Bob の得点を計算
   alice = sum([for i in range(0, local.n, 2) : local.sorted_cards[i]])
   bob   = sum([for i in range(1, local.n, 2) : local.sorted_cards[i]])
 
